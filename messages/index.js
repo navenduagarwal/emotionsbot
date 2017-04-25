ab9a7b1d668039e192d04c7a42e82d75252fa2f7
Navendu Agarwal <navendu.agarwal@gmail.com>
2017-04-25 06:07:50 +0530

    updated server

--- messages/index.js
+++ messages/index.js
@@ -1,7 +1,7 @@
 "use strict";
 var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
-var restify = require('restify');
+var express = require('express');
 var useEmulator = (process.env.NODE_ENV == 'development');
 
 var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
@@ -18,16 +18,18 @@ bot.dialog('/', function (session) {
 });
 
 if (useEmulator) {
-    var server = restify.createServer();
-    server.listen(3978, function() {
+    var server = express();
+    server.set('port',3978);
+    server.listen(server.get('port'), function() {
         console.log('test bot endpont at http://localhost:3978/api/messages');
     });
     server.post('/api/messages', connector.listen());    
 } else {
      // Setup Restify Server
-    var server = restify.createServer();
-    server.listen(process.env.port || process.env.PORT || 3978, function () {
-        console.log('%s listening to %s', server.name, server.url);
+    var server = express();
+    server.set('port', process.env.port || process.env.PORT || 3978);
+    server.listen(server.get('port'), function () {
+        console.log('%s listening to %s',1,server.get('port'));
     });
     server.post('/api/messages', connector.listen());
     module.exports = { default: connector.listen() }
